59e5a281843db3696a1acc0a464cb0f823a4505d
Eric Henderson <henderea@gmail.com>
2018-12-26 12:11:38 -0500

    initial version

---
+++ src/index.js
@@ -0,0 +1,45 @@
+require('./jquery-ui-1.11.4.min.css');
+require('./index.scss');
+const $ = require('jquery');
+window.jQuery = window.$ = $;
+require('./jquery-ui-1.11.4.min');
+const _ = require('lodash');
+require('./fontawesome-all.min');
+
+var published = false;
+
+const hideNotifyDiv = _.debounce(() => $('#notifyDiv').css('opacity', 0), 3000);
+
+function showNotify(text) {
+    $('#notifyText').html(text);
+    $('#notifyDiv').css('opacity', 1);
+    hideNotifyDiv();
+}
+
+$(function() {
+    $('#publishButton').button().click(function() {
+        var $this = $(this);
+        if(!published) {
+            $this.button('disable');
+            $this.button('option', 'label', '<i class="far fa-spinner fa-pulse"></i>');
+            $this.button('refresh');
+            setTimeout(() => {
+                $this.button('option', 'label', 'Unpublish');
+                $this.button('enable');
+                $this.button('refresh');
+                showNotify('Published');
+            }, 1000);
+        } else {
+            $this.button('disable');
+            $this.button('option', 'label', '<i class="far fa-spinner fa-pulse"></i>');
+            $this.button('refresh');
+            setTimeout(() => {
+                $this.button('option', 'label', 'Publish');
+                $this.button('enable');
+                $this.button('refresh');
+                showNotify('Unpublished');
+            }, 1000);
+        }
+        published = !published;
+    });
+});
